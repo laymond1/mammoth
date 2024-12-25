fa7c4cc3ebf5149d873f09be40e44faabc5dc7d8
laymond1 <costor107@gmail.com>
2024-12-25 21:39:32 +0000

    Update Online BFPrompt model with new arugments and evaluation method

--- models/online_bfprompt.py
+++ models/online_bfprompt.py
@@ -15,6 +15,7 @@ from utils.args import add_rehearsal_args, ArgumentParser
 
 from models.utils.online_continual_model import OnlineContinualModel
 from models.prompt_utils.model import PromptModel
+from models.prompt_utils.prompt import label2prompt
 from utils.buffer import Buffer
 
 import wandb
@@ -33,17 +34,20 @@ class OnlineBFPrompt(OnlineContinualModel):
         parser.add_argument('--train_mask', type=bool, default=True,  help='if using the class mask at training')
 
         # G-Prompt parameters
-        parser.add_argument('--g_prompt_layer_idx', type=int, default=[0, 1], nargs="+", help='the layer index of the G-Prompt')
+        parser.add_argument('--g_prompt_layer_idx', type=int, default=[], nargs="*", help='the layer index of the G-Prompt')
         parser.add_argument('--g_prompt_length', type=int, default=10, help='length of G-Prompt')
 
         # E-Prompt parameters
-        parser.add_argument('--e_prompt_layer_idx', type=int, default=[2, 3, 4], nargs="+", help='the layer index of the E-Prompt')
+        parser.add_argument('--e_prompt_layer_idx', type=int, default=[0, 1, 2, 3, 4], nargs="*", help='the layer index of the E-Prompt')
         parser.add_argument('--e_prompt_pool_size', default=10, type=int, help='number of prompts (M in paper)')
         parser.add_argument('--e_prompt_length', type=int, default=40, help='length of E-Prompt')
         parser.add_argument('--top_k', default=1, type=int, help='top k prompts to use (N in paper)')        
         parser.add_argument('--pull_constraint_coeff', type=float, default=1.0, help='Coefficient for the pull constraint term, \
                             controlling the weight of the prompt loss in the total loss calculation')
-
+        parser.add_argument('--same_key_value', type=bool, default=True, help='the same key-value across all layers of the E-Prompt')
+        parser.add_argument('--gt_key_value', type=int, default=0, choices=[0, 1], help='ground truth key-value')
+        parser.add_argument('--prompt_prediction', type=int, default=0, choices=[0, 1], help='prompt prediction with logits')
+        
         # SupCon
         parser.add_argument('--use_supcon', default=False, type=bool)
         parser.add_argument('--temperature', default=0.1, type=float, help='temperature for SupCon loss')
@@ -62,6 +66,8 @@ class OnlineBFPrompt(OnlineContinualModel):
         print("Pretrained on Imagenet 21k and finetuned on ImageNet 1k.")
         print("-" * 20)
 
+
+        args.e_prompt_pool_size = args.n_tasks
         tmp_dataset = get_dataset(args) if dataset is None else dataset
         num_classes = tmp_dataset.N_CLASSES
         # if num_classes % args.e_prompt_pool_size != 0: # 196 / 10 => 6
@@ -173,8 +179,79 @@ class OnlineBFPrompt(OnlineContinualModel):
                 
         return logits, loss_dict
     
-    def online_after_task(self, task_id):
-        pass
+    # TODO: multi-key not considered
+    def online_evaluate(self, test_loader):
+        total_correct, total_num_data, total_loss = 0.0, 0.0, 0.0
+        top_p_correct, pred_p_correct = 0.0, 0.0
+        correct_l = torch.zeros(self.num_classes)
+        num_data_l = torch.zeros(self.num_classes)
+        label = []
+        
+        # Create a mapping from label to index for exposed_classes
+        class_to_idx = {label: idx for idx, label in enumerate(self.exposed_classes)}
+        
+        self.net.eval()
+        with torch.no_grad():
+            for i, data in enumerate(test_loader):
+                x, y = data[0], data[1]
+                # Update y using the mapping
+                for j in range(len(y)):
+                    y[j] = class_to_idx[y[j].item()]
+
+                x = x.to(self.device)
+                y = y.to(self.device)
+
+                # Define true_p_idx before using it
+                true_p_idx = label2prompt(y, cls_per_prompt=self.num_classes//self.net.prompt.e_pool_size, e_pool_size=self.net.prompt.e_pool_size)
+
+                # Prompt prediction with backbone and head classifier 
+                if self.args.gt_key_value:
+                    logits = self.net(x, y) 
+                else:
+                    if self.args.prompt_prediction:
+                        # prev_logits = self.net.vit_head(feats)[:, :self.num_classes]
+                        prev_logits = self.net(x) # top_k
+                        prev_logits = prev_logits + self.mask
+                        _, p_preds = prev_logits.topk(1, 1, True, True) # select prompt with head
+                        # predict logits
+                        logits = self.net(x, p_preds)
+                        # prompt prediction accuracy
+                        pred_p_idx = label2prompt(p_preds, cls_per_prompt=self.num_classes//self.net.prompt.e_pool_size, e_pool_size=self.net.prompt.e_pool_size)
+                        pred_p_correct += torch.sum(pred_p_idx == true_p_idx.unsqueeze(1)).item()
+                    else:
+                        logits = self.net(x)
+                logits = logits + self.mask
+                # top-k prompt prediction accuracy
+                top_p_idx = self.net.prompt.top_k_idx
+                top_p_correct += torch.sum(top_p_idx == true_p_idx.unsqueeze(1)).item()
+
+                # count selected prompt when trianing
+                num = true_p_idx.view(-1).bincount(minlength=self.net.prompt.e_pool_size)
+                self.net.prompt.gt_count += num
+
+                loss = F.cross_entropy(logits, y)
+                pred = torch.argmax(logits, dim=-1)
+                _, _preds = logits.topk(1, 1, True, True) # self.topk: 1
+                total_correct += torch.sum(_preds == y.unsqueeze(1)).item()
+                total_num_data += y.size(0)
+
+                xlabel_cnt, correct_xlabel_cnt = self._interpret_pred(y, pred)
+                correct_l += correct_xlabel_cnt.detach().cpu()
+                num_data_l += xlabel_cnt.detach().cpu()
+
+                total_loss += loss.mean().item()
+                label += y.tolist()
+
+        avg_acc = total_correct / total_num_data
+        avg_loss = total_loss / len(test_loader)
+        cls_acc = (correct_l / (num_data_l + 1e-5)).numpy().tolist()
+        avg_top_p_acc = top_p_correct / total_num_data
+        avg_pred_p_acc = pred_p_correct / total_num_data
+
+        eval_dict = {"avg_loss": avg_loss, "avg_acc": avg_acc, "cls_acc": cls_acc,
+                     "avg_top_p_acc": avg_top_p_acc, "avg_pred_p_acc": avg_pred_p_acc}
+        
+        return eval_dict
     
     def online_after_train(self):
         pass
