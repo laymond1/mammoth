d584a9b74da16a269a73a31fc917faf7feef2262
laymond1 <costor107@gmail.com>
2025-01-08 16:12:06 +0900

    Add train_mask argument to control class masking during training for OnlineER and OnlineSGD

--- models/online_sgd.py
+++ models/online_sgd.py
@@ -28,6 +28,9 @@ class OnlineSgd(OnlineContinualModel):
     
     @staticmethod
     def get_parser(parser) -> ArgumentParser:
+        # Trick
+        parser.add_argument('--train_mask', type=int, default=1, choices=[0, 1], help='if using the class mask at training')
+        # Backbone
         parser.add_argument('--ft_backbone', type=bool, default=0, choices=[0, 1], help='fine-tuning backbone')
         # ETC
         parser.add_argument('--clip_grad', type=float, default=1, help='Clip gradient norm')
@@ -117,8 +120,14 @@ class OnlineSgd(OnlineContinualModel):
         with torch.amp.autocast(device_type=self.device.type, enabled=self.args.use_amp):
             logits = self.net(x, return_outputs=True)
             loss_dict = dict()
-            # mask out unseen classes
-            logits = logits + self.mask
+            # here is the trick to mask out classes of non-current classes
+            non_cur_classes_mask = torch.zeros(self.num_classes, device=self.device) - torch.inf
+            non_cur_classes_mask[y.unique()] = 0
+            # mask out unseen classes and non-current classes
+            if self.args.train_mask:
+                logits = logits + non_cur_classes_mask
+            else:
+                logits = logits + self.mask
             
             ce_loss = self.loss(logits, y)
             loss_dict.update({'total_loss': ce_loss})
