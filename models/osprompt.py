9c33ec85545827aac0647abeb33fbfc947fd0655
laymond1 <costor107@gmail.com>
2025-05-07 00:25:34 +0900

    Update OSPrompt and add PoolFormer model implementation

--- models/osprompt.py
+++ models/osprompt.py
@@ -13,9 +13,10 @@ from datasets import get_dataset
 from utils.args import add_rehearsal_args, ArgumentParser
 
 from models.utils.continual_model import ContinualModel
-from models.prompt_utils.model import PromptModel
-# from models.prompt_utils.model_quantized import PromptModel
+from models.osprompt_utils.model import PromptModel
+from utils.schedulers import CosineSchedule
 from utils.buffer import Buffer
+from utils import parse_str_to_int, binary_to_boolean_type
 
 import wandb
 
@@ -29,7 +30,7 @@ class OneStagePrompt(ContinualModel):
     def get_parser(parser) -> ArgumentParser:
         # Parameters
         parser.add_argument('--vit_type', type=str, default='tiny', choices=['tiny', 'small', 'base'], help='ViT type')
-        parser.add_argument('--query', type=str, default='poolformer', choices=['vit', 'poolformer'], help="choose one of [poolformer]")
+        parser.add_argument('--query', type=str, default='vit', choices=['vit', 'poolformer'], help="choose one of [poolformer]")
         parser.add_argument('--e_prompt_layer_idx', type=int, default=[0, 1, 2, 3, 4], nargs="+", help='the layer index of the E-Prompt')
         parser.add_argument('--e_prompt_pool_size', type=int, default=100, help='pool size')
         parser.add_argument('--e_prompt_length', type=int, default=8, help='prompt length')
@@ -37,11 +38,11 @@ class OneStagePrompt(ContinualModel):
         parser.add_argument('--pull_constraint_coeff', type=float, default=1.0, help='Coefficient(mu) for the pull constraint term, \
                             controlling the weight of the prompt loss in the total loss calculation')
         parser.add_argument('--same_key_value', type=bool, default=False, help='the same key-value across all layers of the E-Prompt')
-        parser.add_argument('--n_splits', type=int, default=None, help='Number of splits for the prompt pool (default: 1).')
 
         # ETC
         parser.add_argument('--clip_grad', type=float, default=1.0, help='Clip gradient norm')
         parser.add_argument('--use_amp', type=bool, default=True, help='Use automatic mixed precision')
+        parser.add_argument('--use_scheduler', type=binary_to_boolean_type, default=True, help='Use scheduler')
 
         return parser
 
@@ -55,8 +56,6 @@ class OneStagePrompt(ContinualModel):
         tmp_dataset = get_dataset(args) if dataset is None else dataset
         num_classes = tmp_dataset.N_CLASSES
         args.n_tasks = tmp_dataset.N_TASKS
-        if args.n_splits is not None:
-            assert num_classes % args.n_splits == 0
         backbone = PromptModel(args, 
                                num_classes=num_classes,
                                pretrained=True, prompt_flag='os',
@@ -72,6 +71,8 @@ class OneStagePrompt(ContinualModel):
             self.opt.zero_grad(set_to_none=True)
             del self.opt
         self.opt = self.get_optimizer()
+        if self.args.use_scheduler:
+            self.scheduler = CosineSchedule(self.opt, K=self.args.n_epochs)
 
     def begin_epoch(self, epoch, dataset):
         self.count = 0
---
+++ models/osprompt_utils/model.py
@@ -0,0 +1,133 @@
+# This code is a reimplementation based on the OSPrompt methodology.
+# This code has been modified for continual learning by Wonseon Lim.
+
+import torch
+import timm
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from models.osprompt_utils.vit import VisionTransformer
+from models.prompt_utils.prompt import L2P, DualPrompt, CodaPrompt, OSPrompt
+
+
+vit_config = {
+    'tiny':  {'embed_dim': 192, 'depth': 12, 'num_heads': 3},
+    'small': {'embed_dim': 384, 'depth': 12, 'num_heads': 6},
+    'base':  {'embed_dim': 768, 'depth': 12, 'num_heads': 12},
+}
+
+
+class PromptModel(nn.Module):
+    def __init__(self, args, num_classes=10, pretrained=False, prompt_flag=False, prompt_param=None):
+        super(PromptModel, self).__init__()
+
+        self.args = args
+        # select prompt method
+        self.num_classes = num_classes
+        self.prompt_flag = prompt_flag
+        # select vit type
+        vit_type = getattr(args, 'vit_type', 'base')
+        if vit_type not in vit_config:
+            raise ValueError(f"Unknown ViT type: {vit_type}")
+
+        cfg = vit_config[vit_type]
+        self.embed_dim = cfg['embed_dim']
+
+        # get feature encoder
+        if pretrained:
+            # load query model
+            if self.args.query == 'vit':
+                zoo_model_query = VisionTransformer(img_size=224, patch_size=16,
+                                                    embed_dim=cfg['embed_dim'],
+                                                    depth=cfg['depth'],
+                                                    num_heads=cfg['num_heads'],
+                                                    ckpt_layer=0, drop_path_rate=0)
+                load_dict = timm.create_model(f'vit_{vit_type}_patch16_224', pretrained=True).state_dict()
+                del load_dict['head.weight']; del load_dict['head.bias']
+                zoo_model_query.load_state_dict(load_dict)
+            elif self.args.query == 'poolformer':
+                print( "Load poolformer fine-tuned on in1k ...")
+                zoo_model_query = timm.create_model('poolformerv2_m36.sail_in1k', pretrained=True, features_only=True)
+            else:
+                NotImplementedError
+            self.feat_query = zoo_model_query
+            self.feat_query.requires_grad_(False)
+
+            # load prompt model
+            self.feat = VisionTransformer(img_size=224, patch_size=16,
+                                        embed_dim=cfg['embed_dim'],
+                                        depth=cfg['depth'],
+                                        num_heads=cfg['num_heads'],
+                                        ckpt_layer=0, drop_path_rate=0)
+
+            pretrained_model = timm.create_model(f'vit_{vit_type}_patch16_224', pretrained=True)
+            load_dict = pretrained_model.state_dict()
+            if 'head.weight' in load_dict:
+                del load_dict['head.weight']
+                del load_dict['head.bias']
+            missing, unexpected = self.feat.load_state_dict(load_dict, strict=False)
+            assert len([m for m in missing if 'head' not in m]) == 0, f"Missing keys: {missing}"
+            assert len(unexpected) == 0, f"Unexpected keys: {unexpected}"
+            # grad false
+            self.feat.requires_grad_(False)
+
+        # classifier
+        self.head = nn.Linear(self.embed_dim, num_classes)
+
+        # create prompting module
+        if self.prompt_flag == 'l2p':
+            self.prompt = L2P(args, self.embed_dim, prompt_param, self.embed_dim) # prompt_param: 30 20 -1
+        elif self.prompt_flag == 'dual':
+            self.prompt = DualPrompt(args, self.embed_dim, prompt_param, self.embed_dim) # prompt_param: 10 40 10
+        elif self.prompt_flag == 'coda':
+            self.prompt = CodaPrompt(args, self.embed_dim, prompt_param, self.embed_dim) # prompt_param: 100 8 0.0
+        elif self.prompt_flag == 'os':
+            self.prompt = OSPrompt(args, self.embed_dim, prompt_param, self.embed_dim) # prompt_param: 100 8 1e-4
+        else:
+            self.prompt = None
+
+        # this is for OS-Prompt
+        self.dset_mean = (0.0, 0.0, 0.0)
+        self.dset_std = (1.0, 1.0, 1.0)
+
+        if self.args.query == 'vit':
+            self.dset_mean_q = (0.0,0.0,0.0)
+            self.dset_std_q = (1.0,1.0,1.0)
+        else:
+            self.dset_mean_q  = timm.data.resolve_model_data_config(zoo_model_query)['mean']
+            self.dset_std_q  = timm.data.resolve_model_data_config(zoo_model_query)['std']
+
+        print ('norm for query: {} /{}'.format(self.dset_mean_q, self.dset_std_q ))
+        
+    def forward(self, x, y=None, q=None, train=False, last=False, feat=False, **kwargs):
+        if last:
+            return self.head(x)
+
+        x_backbone = transforms.Normalize(self.dset_mean, self.dset_std)(x)
+        x_query = transforms.Normalize(self.dset_mean_q, self.dset_std_q)(x)
+
+        if self.prompt is not None:
+            with torch.no_grad():
+                if self.args.query == 'vit':
+                    q, _ = self.feat_query(x_query)
+                    q = q[:,0,:]
+                elif self.args.query in ['poolformer', 'swin']:
+                    q = self.feat_query(x_query)
+                    q = q[-1].mean(-2).mean(-1)
+                else:
+                    q = self.feat_query(x_query)
+            # Forward with prompt
+            out, prompt_loss = self.feat(x_backbone, prompt=self.prompt, q=q, train=train)
+            out = out[:, 0, :]
+        else:
+            out, _ = self.feat(x)
+            out = out[:, 0, :]
+        out = out.view(out.size(0), -1)
+
+        if feat:
+            return out
+        out = self.head(out)
+        if self.prompt is not None and train:
+            return out, prompt_loss
+        else:
+            return out
