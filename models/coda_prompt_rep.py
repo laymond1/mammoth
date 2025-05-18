c08ba9aeeaacf4c31f6e2e1640acfe58c43bda0e
laymond1 <costor107@gmail.com>
2025-05-18 18:20:54 +0900

    feat: Add REP Model with configurations

---
+++ models/coda_prompt_rep.py
@@ -0,0 +1,141 @@
+"""
+CODA-Prompt: COntinual Decomposed Attention-based Prompting
+
+Note:
+    CODA-Prompt USES A CUSTOM BACKBONE: `vit_base_patch16_224`.
+    The backbone is a ViT-B/16 pretrained on Imagenet 21k and finetuned on ImageNet 1k.
+"""
+
+import torch
+import torch.nn.functional as F
+
+from datasets import get_dataset
+from utils.args import add_rehearsal_args, ArgumentParser
+
+from models.utils.continual_model import ContinualModel
+from models.rep_utils.model import PromptModel
+from utils.schedulers import CosineSchedule
+from utils.buffer import Buffer
+from utils import parse_str_to_int, binary_to_boolean_type
+
+import wandb
+
+
+class CodaPromptREP(ContinualModel):
+    """Resource Efficient Prompt."""
+    NAME = 'coda-prompt-rep'
+    COMPATIBILITY = ['class-il', 'domain-il', 'task-il', 'general-continual']
+
+    @staticmethod
+    def get_parser(parser) -> ArgumentParser:
+        # Parameters
+        parser.add_argument('--vit_type', type=str, default='base', choices=['tiny', 'small', 'base'], help='ViT type')
+        parser.add_argument('--e_prompt_layer_idx', type=parse_str_to_int, default=[0, 1, 2, 3, 4], help='the layer index of the E-Prompt')
+        parser.add_argument('--e_prompt_pool_size', type=int, default=100, help='pool size')
+        parser.add_argument('--e_prompt_length', type=int, default=8, help='prompt length')
+        parser.add_argument('--ortho_mu', type=float, default=0.0, help='orthogonal penalty weight') # but it's set to 0.0 becuase of (#issue12)[https://github.com/GT-RIPL/CODA-Prompt/issues/12]
+        parser.add_argument('--pull_constraint_coeff', type=float, default=1.0, help='Coefficient(mu) for the pull constraint term, \
+                            controlling the weight of the prompt loss in the total loss calculation')
+        parser.add_argument('--same_key_value', type=bool, default=False, help='the same key-value across all layers of the E-Prompt')
+        parser.add_argument('--head_epoch_start_ratio', type=float, default=1.0, help='the ratio of the epochs to start training the head')
+        # AToM (Adaptive Token Merging)
+        parser.add_argument('--tome_type', type=str, default='atom', choices=['tome', 'atom'], help='Adaptive Token Merging')
+        parser.add_argument('--r', type=int, default=8, help='the number of tokens to be remained after merging')
+        # ALD (Adaptive Layer Droping)
+        parser.add_argument('--use_ald', type=binary_to_boolean_type, default=True, help='Use Adaptive Layer Droping')
+        parser.add_argument('--theta_min', type=float, default=0.5, help='the threshold to drop the layer')
+        # ETC
+        parser.add_argument('--clip_grad', type=float, default=1.0, help='Clip gradient norm')
+        parser.add_argument('--use_amp', type=bool, default=True, help='Use automatic mixed precision')
+        parser.add_argument('--use_scheduler', type=binary_to_boolean_type, default=True, help='Use scheduler')
+
+        return parser
+
+    def __init__(self, backbone, loss, args, transform, dataset=None):
+        del backbone
+        print("-" * 20)
+        print(f"WARNING: CODA-Prompt USES A CUSTOM BACKBONE: `vit_base_patch16_224`.")
+        print("Pretrained on Imagenet 21k and finetuned on ImageNet 1k.")
+        print("-" * 20)
+
+        tmp_dataset = get_dataset(args) if dataset is None else dataset
+        num_classes = tmp_dataset.N_CLASSES
+        args.n_tasks = tmp_dataset.N_TASKS
+        backbone = PromptModel(args, 
+                               num_classes=num_classes,
+                               pretrained=True, prompt_flag='coda',
+                               prompt_param=[args.e_prompt_pool_size, args.e_prompt_length, args.ortho_mu])
+
+        super().__init__(backbone, loss, args, transform, dataset=dataset)
+        self.scaler = torch.amp.GradScaler(enabled=self.args.use_amp)
+    
+    def begin_task(self, dataset):
+        if self.current_task > 0:
+            self.net.prompt.process_task_count()
+        if hasattr(self, 'opt'):
+            self.opt.zero_grad(set_to_none=True)
+            del self.opt
+        self.opt = self.get_optimizer()
+        if self.args.use_scheduler:
+            self.scheduler = CosineSchedule(self.opt, K=self.args.n_epochs)
+        # reset step
+        self.net.feat._pld_info["step"] = 0
+        # set gamma
+        # num_total_steps = len(dataset.train_loader) * self.args.n_epochs
+        # self.net.feat._pld_info["gamma"] = 100 / num_total_steps # following PLD paper hyp
+        # self.net.feat._pld_info["gamma"] = 0.001 # following deepspeed hyp
+
+    def begin_epoch(self, epoch, dataset):
+        self.count = 0
+        self.running_loss = 0.0
+        self.running_accuracy = 0.0
+
+    def observe(self, inputs, labels, not_aug_inputs, epoch=None):
+        if isinstance(self.device, str):
+            device = torch.device(self.device)
+        else:
+            device = self.device
+
+        # with torch.amp.autocast(device_type=device.type, enabled=self.args.use_amp):
+        if epoch < int(self.args.n_epochs * self.args.head_epoch_start_ratio):
+            logits, loss_prompt = self.net(inputs, train=True)
+        else:
+            with torch.no_grad():
+                feats = self.net(inputs, feat=True, train=False).detach()
+            logits = self.net(feats, last=True)
+            loss_prompt = None
+        # here is the trick to mask out classes of non-current tasks
+        logits[:, :self.n_past_classes] = -float('inf')
+
+        loss = self.loss(logits[:, :self.n_seen_classes], labels)
+        if self.args.pull_constraint_coeff > 0.0 and loss_prompt is not None:
+            loss = loss + self.args.pull_constraint_coeff * loss_prompt.mean()  # the mean is needed for data-parallel (concatenates instead of averaging)
+
+        self.opt.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.get_parameters(), self.args.clip_grad)
+        self.opt.step()
+
+        # self.scaler.scale(loss).backward()
+        # torch.nn.utils.clip_grad_norm_(self.get_parameters(), self.args.clip_grad)
+        # self.scaler.step(self.opt)
+        # self.scaler.update()
+
+        # Calculate accuracy
+        preds = torch.argmax(logits[:, :self.n_seen_classes], dim=1)
+        correct = (preds == labels).sum().item()
+        total = labels.size(0)
+        accuracy = correct / total
+
+        # Update running loss와 accuracy
+        self.count += 1
+        self.running_loss += loss.item()
+        self.running_accuracy += accuracy
+
+        return loss.item()
+    
+    def get_parameters(self):
+        return [p for n, p in self.net.named_parameters() if 'prompt' in n or 'head' in n]
+    
+    def forward(self, x):
+        return self.net(x)[:, :self.n_seen_classes]
---
+++ models/rep_utils/rep/utils.py
@@ -0,0 +1,151 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+
+import time
+import math
+from typing import List, Tuple, Union
+
+import torch
+from tqdm import tqdm
+
+
+def benchmark(
+    model: torch.nn.Module,
+    device: torch.device = 0,
+    input_size: Tuple[int] = (3, 224, 224),
+    batch_size: int = 64,
+    runs: int = 40,
+    throw_out: float = 0.25,
+    use_fp16: bool = False,
+    verbose: bool = False,
+) -> float:
+    """
+    Benchmark the given model with random inputs at the given batch size.
+
+    Args:
+     - model: the module to benchmark
+     - device: the device to use for benchmarking
+     - input_size: the input size to pass to the model (channels, h, w)
+     - batch_size: the batch size to use for evaluation
+     - runs: the number of total runs to do
+     - throw_out: the percentage of runs to throw out at the start of testing
+     - use_fp16: whether or not to benchmark with float16 and autocast
+     - verbose: whether or not to use tqdm to print progress / print throughput at end
+
+    Returns:
+     - the throughput measured in images / second
+    """
+    if not isinstance(device, torch.device):
+        device = torch.device(device)
+    is_cuda = torch.device(device).type == "cuda"
+
+    model = model.eval().to(device)
+    input = torch.rand(batch_size, *input_size, device=device)
+    if use_fp16:
+        input = input.half()
+
+    warm_up = int(runs * throw_out)
+    total = 0
+    start = time.time()
+
+    with torch.autocast(device.type, enabled=use_fp16):
+        with torch.no_grad():
+            for i in tqdm(range(runs), disable=not verbose, desc="Benchmarking"):
+                if i == warm_up:
+                    if is_cuda:
+                        torch.cuda.synchronize()
+                    total = 0
+                    start = time.time()
+
+                model(input)
+                total += batch_size
+
+    if is_cuda:
+        torch.cuda.synchronize()
+
+    end = time.time()
+    elapsed = end - start
+
+    throughput = total / elapsed
+
+    if verbose:
+        print(f"Throughput: {throughput:.2f} im/s")
+
+    return throughput
+
+
+def parse_r(num_layers: int, r: Union[List[int], Tuple[int, float], int]) -> List[int]:
+    """
+    Process a constant r or r schedule into a list for use internally.
+
+    r can take the following forms:
+     - int: A constant number of tokens per layer.
+     - Tuple[int, float]: A pair of r, inflection.
+       Inflection describes there the the reduction / layer should trend
+       upward (+1), downward (-1), or stay constant (0). A value of (r, 0)
+       is as providing a constant r. (r, -1) is what we describe in the paper
+       as "decreasing schedule". Any value between -1 and +1 is accepted.
+     - List[int]: A specific number of tokens per layer. For extreme granularity.
+    """
+    inflect = 0
+    if isinstance(r, list):
+        if len(r) < num_layers:
+            r = r + [0] * (num_layers - len(r))
+        return list(r)
+    elif isinstance(r, tuple):
+        r, inflect = r
+
+    min_val = int(r * (1.0 - inflect))
+    max_val = 2 * r - min_val
+    step = (max_val - min_val) / (num_layers - 1)
+
+    return [int(min_val + step * i) for i in range(num_layers)]
+
+
+def AToM_parse_r(num_layers: int, r: Union[List[int], Tuple[int, float], int]) -> List[int]:
+    """
+    Process a constant r or r schedule into a list for use internally.
+
+    r can take the following forms:
+     - int: A constant number of tokens per layer.
+     - Tuple[int, float]: A pair of r, inflection.
+       Inflection describes there the the reduction / layer should trend
+       upward (+1), downward (-1), or stay constant (0). A value of (r, 0)
+       is as providing a constant r. (r, -1) is what we describe in the paper
+       as "decreasing schedule". Any value between -1 and +1 is accepted.
+     - List[int]: A specific number of tokens per layer. For extreme granularity.
+    """
+    r_max = 2 * r # 16
+    delta = r_max / (num_layers - 1) # 16 / 11 = 1.455
+
+    return [min(int(delta * l), r_max) for l in range(num_layers)]
+
+
+def parse_theta(r_list: List[int], **kwargs) -> List[float]:
+    """
+    Process a constant theta or theta schedule into a list for use internally.
+
+    theta can take the following forms:
+     - int: A constant number of tokens per layer.
+     - List[int]: A specific number of tokens per layer. For extreme granularity.
+    """
+    allowed_keys = {"gamma", "step", "theta_min", "tau"}
+    filtered_kwargs = {k: v for k, v in kwargs.items() if k in allowed_keys}
+    
+    theta_list = [get_layer_drop_prob(r, **filtered_kwargs) for r in r_list]
+
+    return theta_list
+
+
+def get_layer_drop_prob(r, gamma, step, theta_min=0.5, tau=12):
+    """
+    Calculate the drop probability for a given layer and step.
+    """
+    alpha = 0.9 if r >= tau else 1.0
+    prob = alpha * ((1 - theta_min) * math.exp(-gamma * step) + theta_min)
+
+    return prob
