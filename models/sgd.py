a74a74b76cbc4b0e1f7024e5ae3c2478bb7a9202
laymond1 <costor107@gmail.com>
2025-06-24 21:39:51 +0900

    feat: add SGD model configuration and integrate ViT with scheduler options

--- models/sgd.py
+++ models/sgd.py
@@ -8,6 +8,10 @@ This module implements the simplest form of incremental training, i.e., finetuni
 # LICENSE file in the root directory of this source tree.
 
 from models.utils.continual_model import ContinualModel
+from models.vit_utils.model import ViT
+from utils.schedulers import CosineSchedule
+from utils.args import ArgumentParser
+from utils import binary_to_boolean_type
 
 
 class Sgd(ContinualModel):
@@ -18,8 +22,26 @@ class Sgd(ContinualModel):
     NAME = 'sgd'
     COMPATIBILITY = ['class-il', 'domain-il', 'task-il', 'general-continual']
 
+    @staticmethod
+    def get_parser(parser) -> ArgumentParser:
+        """
+        Returns an ArgumentParser object with predefined arguments for the model.
+        """
+        parser.add_argument('--vit_type', type=str, default='tiny', choices=['tiny', 'small', 'base'], help='ViT type')
+        parser.add_argument('--use_scheduler', type=binary_to_boolean_type, default=True, help='Use scheduler')
+
+        return parser
+
     def __init__(self, backbone, loss, args, transform, dataset=None):
+
+        num_classes = dataset.N_CLASSES
+        args.n_tasks = dataset.N_TASKS
+        backbone = ViT(args, num_classes=num_classes, pretrained=True)
         super(Sgd, self).__init__(backbone, loss, args, transform, dataset=dataset)
+
+    def begin_task(self, dataset):
+        if self.args.use_scheduler:
+            self.scheduler = CosineSchedule(self.opt, K=self.args.n_epochs)
 
     def observe(self, inputs, labels, not_aug_inputs, epoch=None, **kwargs):
         """
