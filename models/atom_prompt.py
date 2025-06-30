2517a26148d545b979ab59b575fc608382cb0a41
laymond1 <costor107@gmail.com>
2025-06-30 19:56:29 +0900

    feat: add AToM model and utilities for on-device continual learning

---
+++ models/atom_prompt.py
@@ -0,0 +1,145 @@
+"""
+REP: Resource Efficient Prompt Learning for On-Device Continual Learning
+
+Note:
+    CODA-Prompt USES A CUSTOM BACKBONE: `vit_base_patch16_224`.
+    The backbone is a ViT-B/16 pretrained on Imagenet 21k and finetuned on ImageNet 1k.
+"""
+
+import torch
+import torch.nn.functional as F
+
+from datasets import get_dataset
+from utils.args import add_rehearsal_args, ArgumentParser
+
+from models.utils.continual_model import ContinualModel
+from models.atom_prompt_utils.model import PromptModel
+from utils.schedulers import CosineSchedule
+from utils.buffer import Buffer
+from utils import parse_str_to_int, binary_to_boolean_type
+
+import wandb
+
+
+class AToMPrompt(ContinualModel):
+    """Continual Learning via CODA-Prompt: COntinual Decomposed Attention-based Prompting."""
+    NAME = 'atom-prompt'
+    COMPATIBILITY = ['class-il', 'domain-il', 'task-il', 'general-continual']
+
+    @staticmethod
+    def get_parser(parser) -> ArgumentParser:
+        # Parameters
+        parser.add_argument('--vit_type', type=str, default='tiny', choices=['tiny', 'small', 'base'], help='ViT type')
+        parser.add_argument('--e_prompt_layer_idx', type=int, default=[0, 1, 2, 3, 4], nargs="+", help='the layer index of the E-Prompt')
+        parser.add_argument('--e_prompt_pool_size', type=int, default=100, help='pool size')
+        parser.add_argument('--e_prompt_length', type=int, default=8, help='prompt length')
+        parser.add_argument('--ortho_mu', type=float, default=0.0, help='orthogonal penalty weight') # but it's set to 0.0 becuase of (#issue12)[https://github.com/GT-RIPL/CODA-Prompt/issues/12]
+        parser.add_argument('--pull_constraint_coeff', type=float, default=1.0, help='Coefficient(mu) for the pull constraint term, \
+                            controlling the weight of the prompt loss in the total loss calculation')
+        parser.add_argument('--same_key_value', type=bool, default=False, help='the same key-value across all layers of the E-Prompt')
+        parser.add_argument('--head_epoch_start_ratio', type=float, default=1.0, help='the ratio of the epochs to start training the head')
+
+        # ToMe 
+        parser.add_argument('--r', type=int, help='given a value of r, the prompt_r and query_r are ignored')
+        parser.add_argument('--prompt_r', type=int, help='number of tokens to merge')
+        parser.add_argument('--query_r', type=int, help='number of tokens to merge')
+        # Prompt Sparsity
+        parser.add_argument('--prompt_prompt_sparse', type=binary_to_boolean_type, default=True, help='enable token pruning during prompt forward pass for efficiency')
+        parser.add_argument('--head_prompt_sparse', type=binary_to_boolean_type, default=False, help='enable token pruning during head forward pass for efficiency')
+        parser.add_argument('--test_prompt_sparse', type=binary_to_boolean_type, default=True, help='enable token pruning during test forward pass for efficiency')
+        # Query Sparsity
+        parser.add_argument('--prompt_query_sparse', type=binary_to_boolean_type, default=False, help='enable token pruning during prompt forward pass for efficiency')
+        parser.add_argument('--head_query_sparse', type=binary_to_boolean_type, default=False, help='enable token pruning during head forward pass for efficiency')
+        parser.add_argument('--test_query_sparse', type=binary_to_boolean_type, default=False, help='enable token pruning during test forward pass for efficiency')
+
+        # ETC
+        parser.add_argument('--clip_grad', type=float, default=1.0, help='Clip gradient norm')
+        parser.add_argument('--use_amp', type=bool, default=True, help='Use automatic mixed precision')
+        parser.add_argument('--use_scheduler', type=binary_to_boolean_type, default=True, help='Use scheduler')
+
+        return parser
+
+    def __init__(self, backbone, loss, args, transform, dataset=None):
+        del backbone
+        print("-" * 20)
+        print(f"WARNING: CODA-Prompt USES A CUSTOM BACKBONE: `vit_base_patch16_224`.")
+        print("Pretrained on Imagenet 21k and finetuned on ImageNet 1k.")
+        print("-" * 20)
+
+        tmp_dataset = get_dataset(args) if dataset is None else dataset
+        num_classes = tmp_dataset.N_CLASSES
+        args.n_tasks = tmp_dataset.N_TASKS
+        if args.r is not None:
+            args.prompt_r = args.r
+            args.query_r = args.r
+        backbone = PromptModel(args, 
+                               num_classes=num_classes,
+                               pretrained=True, prompt_flag='coda',
+                               prompt_param=[args.e_prompt_pool_size, args.e_prompt_length, args.ortho_mu])
+
+        super().__init__(backbone, loss, args, transform, dataset=dataset)
+        self.scaler = torch.amp.GradScaler(enabled=self.args.use_amp)
+    
+    def begin_task(self, dataset):
+        if self.current_task > 0:
+            self.net.prompt.process_task_count()
+        if hasattr(self, 'opt'):
+            self.opt.zero_grad(set_to_none=True)
+            del self.opt
+        self.opt = self.get_optimizer()
+        if self.args.use_scheduler:
+            self.scheduler = CosineSchedule(self.opt, K=self.args.n_epochs)
+
+    def begin_epoch(self, epoch, dataset):
+        self.count = 0
+        self.running_loss = 0.0
+        self.running_accuracy = 0.0
+
+    def observe(self, inputs, labels, not_aug_inputs, epoch=None):
+        if isinstance(self.device, str):
+            device = torch.device(self.device)
+        else:
+            device = self.device
+
+        # with torch.amp.autocast(device_type=device.type, enabled=self.args.use_amp):
+        if epoch < int(self.args.n_epochs * self.args.head_epoch_start_ratio):
+            logits, loss_prompt = self.net(inputs, train=True)
+        else:
+            with torch.no_grad():
+                feats = self.net(inputs, feat=True, train=False).detach()
+            logits = self.net(feats, last=True)
+            loss_prompt = None
+        # here is the trick to mask out classes of non-current tasks
+        logits[:, :self.n_past_classes] = -float('inf')
+
+        loss = self.loss(logits[:, :self.n_seen_classes], labels)
+        if self.args.pull_constraint_coeff > 0.0 and loss_prompt is not None:
+            loss = loss + self.args.pull_constraint_coeff * loss_prompt.mean()  # the mean is needed for data-parallel (concatenates instead of averaging)
+
+        self.opt.zero_grad()
+        loss.backward()
+        grad_total_norm = torch.nn.utils.clip_grad_norm_(self.get_parameters(), self.args.clip_grad)
+        self.opt.step()
+        # self.scaler.scale(loss).backward()
+        # torch.nn.utils.clip_grad_norm_(self.get_parameters(), self.args.clip_grad)
+        # self.scaler.step(self.opt)
+        # self.scaler.update()
+
+        # Calculate accuracy
+        preds = torch.argmax(logits[:, :self.n_seen_classes], dim=1)
+        correct = (preds == labels).sum().item()
+        total = labels.size(0)
+        accuracy = correct / total
+
+        # Update running loss와 accuracy
+        self.count += 1
+        self.running_loss += loss.item()
+        self.running_accuracy += accuracy
+
+        return loss.item()
+    
+    def get_parameters(self):
+        return [p for n, p in self.net.named_parameters() if 'prompt' in n or 'head' in n]
+    
+    def forward(self, x):
+        return self.net(x)[:, :self.n_seen_classes]
---
+++ models/atom_prompt_utils/atom.py
@@ -0,0 +1,244 @@
+# This code is a reimplementation based on the ToMe methodology.
+# This code has been modified for on-device continual learning by Wonseon Lim.
+
+from typing import Tuple
+
+import torch
+from models.prompt_utils.vit import Attention, Block, VisionTransformer
+from models.atom_prompt_utils.merge import bipartite_soft_matching, merge_source, merge_wavg
+from models.atom_prompt_utils.utils import AToM_parse_r
+
+
+class AToMBlock(Block):
+    """
+    Modifications:
+     - Apply Adaptive Token Merging (AToM) between the attention and mlp blocks
+     - Compute and propogate token size and potentially the token sources.
+    """
+
+    def _drop_path1(self, x):
+        return self.drop_path1(x) if hasattr(self, "drop_path1") else self.drop_path(x)
+
+    def _drop_path2(self, x):
+        return self.drop_path2(x) if hasattr(self, "drop_path2") else self.drop_path(x)
+
+    def forward(self, x: torch.Tensor, register_hook: bool = False, prompt: torch.Tensor = None, sparse: bool = True) -> torch.Tensor:
+        # Note: this is copied from timm.models.vision_transformer.Block with modifications.
+        attn_size = self._atom_info["size"] if self._atom_info["prop_attn"] else None
+        # Full Token Forward
+        if not sparse:
+            x_attn = self.attn(self.norm1(x), register_hook=register_hook, prompt=prompt, size=attn_size, sparse=sparse)
+            metric = None
+            x = x + self._drop_path1(x_attn)
+            x = x + self._drop_path2(self.mlp(self.norm2(x)))
+            return x
+        # Sparse Token Forward
+        else:
+            x_attn, metric = self.attn(self.norm1(x), register_hook=register_hook, prompt=prompt, size=attn_size)
+            x = x + self._drop_path1(x_attn)
+
+            r = self._atom_info["r"].pop(0)
+            if r > 0:
+                # Apply AToM here
+                merge, _ = bipartite_soft_matching(
+                    metric,
+                    r,
+                    self._atom_info["class_token"],
+                    self._atom_info["distill_token"],
+                )
+                if self._atom_info["trace_source"]:
+                    self._atom_info["source"] = merge_source(
+                        merge, x, self._atom_info["source"]
+                    )
+                x, self._atom_info["size"] = merge_wavg(merge, x, self._atom_info["size"])
+
+            x = x + self._drop_path2(self.mlp(self.norm2(x)))
+            return x
+
+
+class AToMAttention(Attention):
+    """
+    Modifications:
+     - Apply proportional attention
+     - Return the mean of k over heads from attention
+    """
+
+    def forward(
+        self, x: torch.Tensor, register_hook: bool = False, prompt: torch.Tensor = None, size: torch.Tensor = None, sparse: bool = True
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Full Token Forward
+        if not sparse:
+            return super().forward(x, register_hook=register_hook, prompt=prompt)
+
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        if prompt is not None:
+            # import ipdb; ipdb.set_trace()
+            pk, pv = prompt
+            pk = pk.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+            pv = pv.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+            k = torch.cat((pk,k), dim=2)
+            v = torch.cat((pv,v), dim=2)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        # Apply proportional attention
+        if size is not None:
+            if prompt is not None:
+                attn[:, :, :, 4:] = attn[:, :, :, 4:] + size.log()[:, None, None, :, 0]
+            else:
+                attn = attn + size.log()[:, None, None, :, 0]
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        if register_hook:
+            self.save_attention_map(attn)
+            # attn.register_hook(self.save_attn_gradients)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        if prompt is not None:
+            k = k[:, :, 4:, :]
+        # Return k as well here
+        return x, k.mean(1)
+
+
+def make_atom_class(transformer_class):
+    class AToMVisionTransformer(transformer_class):
+        """
+        Modifications:
+        - Initialize r, token size, and token sources.
+        """
+
+        def forward(self, x, register_blk=-1, prompt=None, q=None, train=False, feat=False) -> torch.Tensor:
+            if prompt is not None:
+                self._atom_info["r"] = AToM_parse_r(len(self.blocks), self.prompt_r)
+            else:
+                self._atom_info["r"] = AToM_parse_r(len(self.blocks), self.query_r)
+            self._atom_info["size"] = None
+            self._atom_info["source"] = None
+
+            B = x.shape[0]
+            x = self.patch_embed(x)
+
+            cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+            x = torch.cat((cls_tokens, x), dim=1)
+    
+            x = x + self.pos_embed[:,:x.size(1),:]
+            x = self.pos_drop(x)
+
+            prompt_loss = torch.zeros((1,), requires_grad=True).to(x.device)
+
+            for i,blk in enumerate(self.blocks):
+
+                if prompt is not None:
+                    if train:
+                        p_list, loss, x = prompt.forward(q, i, x, train=True)
+                        prompt_loss += loss
+                    else:
+                        p_list, _, x = prompt.forward(q, i, x, train=False)
+                
+                else:
+                    p_list = None
+
+                # Forward for prompt 
+                if prompt is not None:
+                    if train:
+                        # Prompt Forward for Prompt Training
+                        if self.prompt_prompt_sparse:
+                            # Sparse Token Forward (Train for Prompt)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        else:
+                            # Full Token Forward (Train for Prompt)
+                            x = blk(x, register_blk==i, prompt=p_list, sparse=False)
+                    else:
+                        # Prompt Forward for Inference or Classifier Training
+                        if self.head_prompt_sparse and feat:
+                            # Sparse Token Forward (Train for Classifier, feat=True)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        elif self.test_prompt_sparse:
+                            # Sparse Token Forward (Inference)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        else:
+                            # Full Token Forward (Inference)
+                            x = blk(x, register_blk==i, prompt=p_list, sparse=False)
+                # Forward for query
+                else:
+                    if train:
+                        # Query Forward for Prompt Training
+                        if self.prompt_query_sparse:
+                            # Sparse Token Forward (Train for Prompt)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        else:
+                            # Full Token Forward (Train for Prompt)
+                            x = blk(x, register_blk==i, prompt=p_list, sparse=False)
+                    else:
+                        # Query Forward for Inference or Classifier Training
+                        if self.head_query_sparse and feat:
+                            # Sparse Token Forward (Train for Classifier, feat=True)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        elif self.test_query_sparse:
+                            # Sparse Token Forward (Inference)
+                            x = blk(x, register_blk==i, prompt=p_list)
+                        else:
+                            # Full Token Forward (Inference)
+                            x = blk(x, register_blk==i, prompt=p_list, sparse=False)
+
+            x = self.norm(x)
+
+            if prompt is not None:
+                prompt_loss /= len(prompt.e_layers)
+            
+            return x, prompt_loss
+
+    return AToMVisionTransformer
+
+
+def apply_patch(
+    model: VisionTransformer, trace_source: bool = False, prop_attn: bool = True
+):
+    """
+    Applies AToM to this transformer. Afterward, set r using model.r.
+
+    If you want to know the source of each token (e.g., for visualization), set trace_source = true.
+    The sources will be available at model._atom_info["source"] afterward.
+
+    For proportional attention, set prop_attn to True. This is only necessary when evaluating models off
+    the shelf. For trianing and for evaluating MAE models off the self set this to be False.
+    """
+    AToMVisionTransformer = make_atom_class(model.__class__)
+
+    model.__class__ = AToMVisionTransformer
+    model.prompt_r = 0
+    model.query_r = 0
+    model.prompt_prompt_sparse = False
+    model.head_prompt_sparse = False
+    model.test_prompt_sparse = False
+    model.prompt_query_sparse = False
+    model.head_query_sparse = False
+    model.test_query_sparse = False
+    model._atom_info = {
+        "prompt_r": model.prompt_r,
+        "query_r": model.query_r,
+        "size": None,
+        "source": None,
+        "trace_source": trace_source,
+        "prop_attn": prop_attn,
+        "class_token": model.cls_token is not None,
+        "distill_token": False
+    }
+
+    if hasattr(model, "dist_token") and model.dist_token is not None:
+        model._atom_info["distill_token"] = True
+
+    for module in model.modules():
+        if isinstance(module, Block):
+            module.__class__ = AToMBlock
+            module._atom_info = model._atom_info
+        elif isinstance(module, Attention):
+            module.__class__ = AToMAttention
